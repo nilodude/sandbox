293f309e4df93cd9489d29b55729bd6bbf17427d
nilodude <alphone@protonmail.com>
2022-12-08 16:19:13 +0100

    still need to improve air roll and stop putin lights

--- src/client/client.ts
+++ src/client/client.ts
@@ -19,35 +19,49 @@ renderer.setSize(size[0], size[1])
 
 //SCENE
 const scene = new THREE.Scene()
-const groundSize = 500;
+const groundSize = 1000;
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.3)
 scene.add(ambientLight)
 
-const spotlight = new THREE.SpotLight(0x0000ff, 2,500, 3*  Math.PI, 1)
+const spotlight = new THREE.SpotLight(0x5522aa, 2,500, 3*  Math.PI, 1)
 spotlight.position.set(0, 0.5, 0)
 // spotlight.target.position.set(0, 0, 0)  //para que parezca el faro del coche se deja de apuntar al centro
 spotlight.castShadow = true
+
+
+const rectLight = new THREE.RectAreaLight( 0xffff00, 1000,  1000, 0.2);
+rectLight.position.set( 0, 30, 0 );
+rectLight.lookAt( 0, 0, 0 );
+scene.add( rectLight )
+const rectLight1 = new THREE.RectAreaLight( 0xffff00, 1000,  1000, 0.2);
+rectLight1.position.set( 0, 30, 250 );
+rectLight1.lookAt( 0, 0, 0 );
+scene.add( rectLight1 )
+const rectLight2 = new THREE.RectAreaLight( 0xffff00, 1000,  1000, 0.2);
+rectLight2.position.set( 0, 30, -250 );
+rectLight2.lookAt( 0, 0, 0 );
+scene.add( rectLight2 )
 
 const numLights = 12;
 const lightsPerSide = numLights/4;
 const spacing = groundSize/lightsPerSide;
 
 for(let i=0;i<=lightsPerSide;i++){
-    const spotlight1 = new THREE.SpotLight(0xffffff, 2, 0, Math.PI / 8, 1)
+    const spotlight1 = new THREE.SpotLight(0xffffff, 10, 1000, Math.PI / 8, 1)
     spotlight1.position.set(groundSize/2, 5,(groundSize/2)-i*spacing ) 
     spotlight1.target.position.set(0, 0, 0)
     
-    const spotlight2 = new THREE.SpotLight(0xffffff, 2, 0, Math.PI / 8, 1)
+    const spotlight2 = new THREE.SpotLight(0xffffff, 10, 1000, Math.PI / 8, 1)
     spotlight2.position.set(-groundSize/2, 5,(groundSize/2)-i*spacing ) 
     spotlight2.target.position.set(0, 0, 0)
     
-    const spotlight3 = new THREE.SpotLight(0xffffff, 2, 0, Math.PI / 8, 1)
+    const spotlight3 = new THREE.SpotLight(0xffffff, 10, 1000, Math.PI / 8, 1)
     spotlight3.position.set(-(groundSize/2)+i*spacing, 5,groundSize/2) 
     spotlight3.target.position.set(0, 0, 0)
 
-    const spotlight4 = new THREE.SpotLight(0xffffff, 2, 0, Math.PI / 8, 1)
+    const spotlight4 = new THREE.SpotLight(0xffffff, 10, 1000, Math.PI / 8, 1)
     spotlight4.position.set(-(groundSize/2)+i*spacing, 5,-groundSize/2) 
-    spotlight4.target.position.set(0, 0, 0)
+    // spotlight4.target.position.set(0, 0, 0)
     
     scene.add(spotlight1)
     scene.add(spotlight2)
@@ -77,7 +91,7 @@ scene.add(spotlight)
 //GROUND MESH
 const groundGeo = new THREE.PlaneGeometry(groundSize, groundSize);
 const groundMat = new THREE.MeshPhysicalMaterial({ 
-	color: 0xff00ff,
+	color: 0xaa00ff,
 	side: THREE.FrontSide,
 	wireframe: false,
     sheenRoughness: 0.001,
